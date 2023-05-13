08d6cf2d244bd94a58386dbb9d732ab50f3228bf
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-05-13 17:27:03 +0900

    simplify code

--- clientA.py
+++ clientA.py
@@ -4,6 +4,7 @@ import time
 import socket
 
 import p2p_communicate
+
 client_socket = None
 msgs = []
 l_ip_address = {'Keyhole':'localhost', 'Kamina':'localhost'}
@@ -11,38 +12,45 @@ l_port = {'Keyhole':12345, 'Kamina':19000}
 
 def first_contact(client_name):
 	global client_socket
-	ip_address = l_ip_address[client_name]
-	port = l_port[client_name]
-	try:
-		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		
-		client_socket.connect((ip_address, port))
-		
-		data = 'succesufull connection'
-		
-		client_socket.sendall(data.encode())
-		
-		received_data = client_socket.recv(1024).decode()
-		print(received_data)
-	except clientNotFound:
-		print('client not found')
+	if client_name in l_ip_address and client_name in l_port:
+		ip_address = l_ip_address[client_name]
+		port = l_port[client_name]
+		try:
+			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			client_socket.connect((ip_address, port))
+			data = 'succesufull connection'
+			print(data)
+			client_socket.sendall(data.encode())
+			received_data = client_socket.recv(1024).decode()
+			print(received_data)
+		except Exception as e:
+			print('Error connecting to client:', e)
+	else:
+		print('Client not found.')
 
 def send():
 	global client_socket
 	def handle_return(event):
 		msg = entry.get()
-		if client_socket == None:
-			first_contact(msg)
+		if client_socket is None:
+			try:
+				first_contact(msg)
+				label = tk.Label(root, text="Enter your message:")
+			except Exception as e:
+				print('Error not found opponent:', e)
 		else:
-			response = msg.encode()
-			client_socket.sendall(response)
-			print(msg)
-			msgs.append(msg)
+			try:
+				response = msg.encode()
+				client_socket.sendall(response)
+				print('you:', msg)
+				msgs.append(msg)
+			except Exception as e:
+				print('Error sending message:', e)
 		entry.delete(0, tk.END)
 
 	root = tk.Tk()
 
-	label = tk.Label(root, text="Enter your message:")
+	label = tk.Label(root, text="Enter opponent name:")
 	label.pack()
 
 	entry = tk.Entry(root)
@@ -56,29 +64,24 @@ def send():
 
 def listen():
 	global client_socket
-	HOST = 'localhost'
-	PORT = 12345
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind((HOST, PORT))
-	server_socket.listen()
+	your_name = ''
+	while your_name not in l_ip_address or your_name not in l_port:
+		your_name = input('Tell me your name in advance: ')
 	
+	y_ip_address = l_ip_address[your_name]
+	y_port = l_port[your_name]
+
+	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server_socket.bind((y_ip_address, y_port))
+	server_socket.listen()
+
 	client_socket, client_address = server_socket.accept()
 	print(f'Connection from {client_address}')
-		
+
 	while True:
-
-		# Receive data from the client
 		data = client_socket.recv(1024).decode()
-		print(f'Received: {data}')
+		print('opponent:', data)
 
-		# Send a response back to the client
-		#client_socket.sendall(data.encode())
-		#print(f"Sent: {data}")
-
-		# Close the client socket
-		#client_socket.close()
-def message_log():
-	print(msg)
 
 thread1 = threading.Thread(target=send)
 thread2 = threading.Thread(target=listen)
