031340e03b14aa8dbc707c9b7b65be0ff7b2513b
Keyhole-Koro <130579219+Keyhole-Koro@users.noreply.github.com>
2023-05-14 00:02:14 +0900

    brush up structure

--- clientA.py
+++ clientA.py
@@ -1,28 +1,21 @@
 import tkinter as tk
 import threading
-import time
 import socket
 
 import p2p_communicate
 
+client_info = {'Keyhole': ('localhost', 12345), 'Kamina': ('localhost', 19000)}
 client_socket = None
-msgs = []
-l_ip_address = {'Keyhole':'localhost', 'Kamina':'localhost'}
-l_port = {'Keyhole':12345, 'Kamina':19000}
-
 def first_contact(client_name):
 	global client_socket
-	if client_name in l_ip_address and client_name in l_port:
-		ip_address = l_ip_address[client_name]
-		port = l_port[client_name]
+	if client_name in client_info:
+		ip_address, port = client_info[client_name]
 		try:
 			client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			client_socket.connect((ip_address, port))
-			data = 'succesufull connection'
-			print(data)
+			print('Successful connection')
+			data = 'Successful connection'
 			client_socket.sendall(data.encode())
-			received_data = client_socket.recv(1024).decode()
-			print(received_data)
 		except Exception as e:
 			print('Error connecting to client:', e)
 	else:
@@ -35,63 +28,56 @@ def send():
 		if client_socket is None:
 			try:
 				first_contact(msg)
-				label = tk.Label(root, text="Enter your message:")
 			except Exception as e:
 				print('Error not found opponent:', e)
 		else:
 			try:
 				response = msg.encode()
 				client_socket.sendall(response)
-				print('you:', msg)
-				msgs.append(msg)
+				print('You:', msg)
 			except Exception as e:
 				print('Error sending message:', e)
 		entry.delete(0, tk.END)
 
 	root = tk.Tk()
-
-	label = tk.Label(root, text="Enter opponent name:")
+	label = tk.Label(root, text="Enter your message:")
 	label.pack()
-
 	entry = tk.Entry(root)
 	entry.pack()
-
 	entry.bind('<Return>', handle_return)
-
 	root.mainloop()
 
-	return msgs
-
-def listen():
+def first_listen():
 	global client_socket
-	your_name = ''
-	while True:
-		your_name = input('Tell me your name in advance: ')
-		if your_name in l_ip_address and your_name in l_port:
-			break
-		else:
-			print("Error: Invalid name.")
-	
-	y_ip_address = l_ip_address[your_name]
-	y_port = l_port[your_name]
-
+	your_name = input('Tell me your name in advance: ')
+	if your_name in client_info:
+		y_ip_address, y_port = client_info[your_name]
+	else:
+		print('Invalid name')
+		return
 	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server_socket.bind((y_ip_address, y_port))
 	server_socket.listen()
-
 	client_socket, client_address = server_socket.accept()
 	print(f'Connection from {client_address}')
 
+
+def listening():
+	global client_socket
 	while True:
-		data = client_socket.recv(1024).decode()
-		print('opponent:', data)
-
-
+		try:
+			data = client_socket.recv(1024).decode()
+			print('opponent:', data)
+		except Exception:
+				pass
 thread1 = threading.Thread(target=send)
-thread2 = threading.Thread(target=listen)
+thread2 = threading.Thread(target=first_listen)
+listening = threading.Thread(target=listening)
 
 thread1.start()
 thread2.start()
+listening.start()
 
 thread1.join()
 thread2.join()
+listening.join()
